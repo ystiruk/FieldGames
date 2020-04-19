5a6bd7cbba287b0b82c4409b9721653751fa0c8f
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-04-19 19:49:25 +0300

    Add IWordProvider

--- source/WordGames/Sandbox/WordDictionary.cs
+++ source/WordGames/Sandbox/WordDictionary.cs
@@ -4,10 +4,9 @@ using System.Text;
 
 namespace Sandbox
 {
-    class WordDictionary
+    public class WordDictionary : IWordProvider
     {
         private const string _alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        //private readonly WordsBoundaries[] searchBoundaries;
 
         public List<string> Words { get; }
 
@@ -18,39 +17,11 @@ namespace Sandbox
             using (var sReader = new StreamReader(path, Encoding.UTF8))
                 while (!sReader.EndOfStream)
                     Words.Add(sReader.ReadLine());
+        }
 
-            //int[] positions = Enumerable.Repeat(-1, _alphabet.Length).ToArray();
-            //for (int i = 0; i < _alphabet.Length; i++)
-            //{
-            //    for (int j = 0; j < Words.Count; j++)
-            //    {
-            //        var firstLetter = Words[j][0];
-            //        if (firstLetter == _alphabet[i])
-            //        {
-            //            positions[i] = j;
-            //            break;
-            //        }
-            //    }
-            //}
-
-            //searchBoundaries = new WordsBoundaries[_alphabet.Length];
+        public bool Contains(string word)
+        {
+            return Words.Contains(word);
         }
     }
-
-    //struct WordsBoundaries
-    //{
-    //    public char Letter { get; }
-    //    public int StartIndex { get; }
-    //    public int EndIndex { get; }
-
-    //    public WordsBoundaries(char letter, int start, int end)
-    //    {
-    //        if (start >= end)
-    //            throw new ArgumentException($"Start position cannot be greater that End position.");
-
-    //        Letter = letter;
-    //        StartIndex = start;
-    //        EndIndex = end;
-    //    }
-    //}
 }
