8072534f2d095006df1779bd22e9589c7db19fcf
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-04-22 15:51:00 +0300

    Add Switch extension method with tests

---
+++ source/FieldGames/FieldGames.Core/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FieldGames.Core
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Switch<T>(this IList<T> source)
+        {
+            int i = 0;
+            while(true)
+            {
+                if (source.Count == 0) throw new Exception(nameof(source));
+                
+                if (i == source.Count) i = 0;
+                yield return source[i++];
+            }
+        }
+    }
+}
---
+++ test/FieldGames/FieldGames.Core.Tests/ExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FieldGames.Core.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void SwitchCollectionWithOneItemReturnsThatItem()
+        {
+            IList<int> source = new List<int>() { 1 };
+            IList<int> expectedSequence = new List<int>() { 1, 1, 1, 1, 1 };
+
+            var switcher = source.Switch();
+
+            var actualSequence = switcher.Take(5);
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(actualSequence));
+        }
+
+        [TestMethod]
+        public void SwitchCollectionWithThreeItemsReturnsTheseItemsInRightOrder()
+        {
+            IList<int> source = new List<int>() { 1, 2, 3 };
+
+            var switcher = source.Switch();
+
+            var actualSequence = switcher.Take(3);
+
+            Assert.IsTrue(source.SequenceEqual(actualSequence));
+        }
+
+        [TestMethod]
+        public void SwitchSupportsRemoveElementFromCollection()
+        {
+            IList<int> source = new List<int>() { 1, 2, 3 };
+            IList<int> expectedSequence = new List<int>() {
+                1, 2, 3,
+                1, 3
+            };
+
+            var switcher = source.Switch();
+
+            var actualSequence = new List<int>();
+            actualSequence.AddRange(switcher.Take(3));
+            source.Remove(2);
+            actualSequence.AddRange(switcher.Take(2));
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(actualSequence));
+        }
+
+        [TestMethod]
+        public void SwitchSupportsAddElementToCollection()
+        {
+            IList<int> source = new List<int>() { 1, 2, 3 };
+            IList<int> expectedSequence = new List<int>() {
+                1, 2, 3,
+                1, 2, 3, 4
+            };
+
+            var switcher = source.Switch();
+
+            List<int> actualSequence = new List<int>();
+            actualSequence.AddRange(switcher.Take(3));
+            source.Add(4);
+            actualSequence.AddRange(switcher.Take(4));
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(actualSequence));
+        }
+
+        [TestMethod]
+        public void SwitchSupportsInsertElementToCollection()
+        {
+            IList<int> source = new List<int>() { 1, 2, 3 };
+            IList<int> expectedSequence = new List<int>() {
+                1, 2, 3,
+                1, 0, 2, 3
+            };
+
+            var switcher = source.Switch();
+
+            var actualSequence = new List<int>();
+            actualSequence.AddRange(switcher.Take(3));
+            source.Insert(1, 0);
+            actualSequence.AddRange(switcher.Take(4));
+
+            Assert.IsTrue(expectedSequence.SequenceEqual(actualSequence));
+        }
+    }
+}
